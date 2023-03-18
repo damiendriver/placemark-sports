f6e042f3113306aa4b0ccf8e0e095ca133261236
Damien Driver <97164055+damiendriver@users.noreply.github.com>
2023-03-18 13:23:57 +0000

    create simple api

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { fileURLToPath } from "url";
 import { accountsController } from "./controllers/accounts-controller.js";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import { apiRoutes } from "./api-routes.js";
 
 const result = dotenv.config();
 if (result.error) {
@@ -55,6 +56,7 @@ async function init() {
 
   db.init("mongo");
   server.route(webRoutes);
+  server.route(apiRoutes);
   await server.start();
   console.log("Server running on %s", server.info.uri);
 }
