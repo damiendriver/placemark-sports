fc412de73f698da69ed804a916abf5278232c3c0
Damien Driver <97164055+damiendriver@users.noreply.github.com>
2023-05-24 21:58:22 +0100

    Add files via upload

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -5,41 +5,41 @@ export const seedData = {
         firstName: "Homer",
         lastName: "Simpson",
         email: "homer@simpson.com",
-        password: "secret"
+        password: "$2a$10$HnapEWWKQQ0.0Ft33nXxtuJ0fsmd9ywYswEzT4C.6Ul5y6cOU.UEat"
       },
       marge: {
         firstName: "Marge",
         lastName: "Simpson",
         email: "marge@simpson.com",
-        password: "secret"
+        password: "$2a$10$D3TWK8MSAh63Lhk4gIFSJ.ekM368J88qJRUYSbuBfzffex6B0dFwK"
       },
       bart: {
         firstName: "Bart",
         lastName: "Simpson",
         email: "bart@simpson.com",
-        password: "secret"
+        password: "$2a$10$uqnPlgyVjBEOtcgMhn/WRO3FwW4T9gX36tTB4RqpGeB/LkCR3YQuO"
       }
     },
     sportgrounds: {
       _model: "Sportground",
       soccer: {
         title: "Soccer",
-        //img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244159/z3kdkwjeizief7kkzqvn.jpg",
+        // img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244159/z3kdkwjeizief7kkzqvn.jpg",
         userid: "->users.bart"
       },
       gaa: {
         title: "GAA",
-        //img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244177/chjny7efs3az4yfqcovb.jpg",
+        // img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244177/chjny7efs3az4yfqcovb.jpg",
         userid: "->users.bart"
       },
       rugby: {
         title: "Rugby",
-        //img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244138/xzdfknx8moun7i2fwnvq.jpg",
+        // img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244138/xzdfknx8moun7i2fwnvq.jpg",
         userid: "->users.homer"
       },
       golf: {
         title: "Golf",
-        //img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244117/ukbz9mtr704mopi8p7pv.jpg",
+        // img: "https://res.cloudinary.com/dqu1cfm2e/image/upload/v1679244117/ukbz9mtr704mopi8p7pv.jpg",
         userid: "->users.homer"
       },
     },
